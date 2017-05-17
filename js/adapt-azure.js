667521c3ad63eb73519a62b2568c54759d3113a6
Mike Stevens <mesgraphix@gmail.com>
2017-05-17 16:00:53 -0400

    Update adapt-azure.js

--- js/adapt-azure.js
+++ js/adapt-azure.js
@@ -58,14 +58,14 @@ define(function(require) {
 
         postRender: function() {
 
-            //CODE BELOW NEEDS BE ADDED INTO THE THEME FOR THIS TO WORK
-            /* JAVASCRIPT FOR AZURE PLAYER <script id="azurejs" src="https://amp.azure.net/libs/amp/1.7.4/azuremediaplayer.min.js"></script>*/
+            //ADD THE MIN.JS AND MIN.CSS TO THE THEME TO GET IT TO WORK
+            /* JAVASCRIPT FOR AZURE PLAYER <script id="azurejs" src="https://amp.azure.net/libs/amp/1.8.3/azuremediaplayer.min.js"></script>*/
             //$( "script#azurejs" ).remove();
-            //$( "head" ).append("<script id=\"azurejs\" src=\"https://amp.azure.net/libs/amp/1.7.4/azuremediaplayer.min.js\"></script>");
+            //$( "head" ).append("<script id=\"azurejs\" src=\"https://amp.azure.net/libs/amp/1.8.3/azuremediaplayer.min.js\"></script>");
 
-            /* CSS FOR AZURE PLAYER <link rel="stylesheet" id="azurecss" href="https://amp.azure.net/libs/amp/1.7.4/skins/amp-default/azuremediaplayer.min.css"> */
+            /* CSS FOR AZURE PLAYER <link rel="stylesheet" id="azurecss" href="https://amp.azure.net/libs/amp/1.8.3/skins/amp-default/azuremediaplayer.min.css"> */
             //$( "link#azurecss" ).remove();
-            //$( "head" ).append("<link rel=\"stylesheet\" id=\"azurecss\" href=\"https://amp.azure.net/libs/amp/1.7.4/skins/amp-default/azuremediaplayer.min.css\">");
+            //$( "head" ).append("<link rel=\"stylesheet\" id=\"azurecss\" href=\"https://amp.azure.net/libs/amp/1.8.3/skins/amp-default/azuremediaplayer.min.css\">");
 
 
             //FOR HTML/HBS Paramenters: https://developers.google.com/youtube/player_parameters
